1922185b77db9d34501545903bec9e8bce88bd19
Todor Grigorov <todorgrigorov89@gmail.com>
2020-12-15 12:26:47 +0200

    Add blogPosts tests

---
+++ Tests/HomeBook.Services.Data.Tests/ServicesTests/BlogPostsServiceTests.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeBook.Services.Data.Tests.ServicesTests
+{
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using HomeBook.Services.Data.BlogPosts;
+    using HomeBook.Web.ViewModels.BlogPosts;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Xunit;
+
+    public class BlogPostsServiceTests : BaseServiceTests
+    {
+        private IBlogPostsService Service => this.ServiceProvider.GetRequiredService<IBlogPostsService>();
+
+        [Fact]
+        public async Task TestAddAsyncApartmentShouldWorkCorrectly()
+        {
+            IFormFile file = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes("This is a dummy file")), 0, 0, "Data", "dummy.img");
+            await this.Service.AddAsync(new BlogPostInputModel
+            {
+                Author = "Test",
+                Title = "Test test",
+                Content = "Test test Test test Test test",
+                Image = file,
+            });
+            Assert.Equal(1, this.DbContext.BlogPosts.Count());
+        }
+    }
+}
